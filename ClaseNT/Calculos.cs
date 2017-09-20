168a8b4c84ed357ac7f44bdf63501a852089b063
Daniela Montoya <danimdzj22@gmail.com>
2017-09-19 19:29:30 -0500

    Operacion Suma

---
+++ ClaseNT/Calculos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseNT
+{
+    class Calculos
+    {
+        //Atributos (campos: Tipo de dato y modificador de acceso)
+        public short num1, num2;
+
+        //Metodo (Estructura orientada a objetos) modificador de acceso (public), retorno(void), nombre(sumar)
+        short sumarNumeros = 0;
+
+        public void sumar(short numero1, short numero2)
+        {
+            sumarNumeros = (short) (numero1 + numero2);
+        }
+
+        public void mostrarResultados()
+        {
+            Console.WriteLine("Total: " + sumarNumeros);
+        }
+
+    }
+}
---
+++ ClaseNT/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseNT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numero uno: ");
+            short n1 = short.Parse(Console.ReadLine()); //Con esta linea se lleva el código
+
+            Console.WriteLine("Numero dos: ");
+            short n2 = short.Parse(Console.ReadLine());
+
+            //Instanciar la clase
+            Calculos realizarOperacion = new Calculos(); //Constructor (Calculos() )
+
+            //utilizamos el objeto (metodo aparece con un cubo (morado))
+            realizarOperacion.num1 = n1;
+            realizarOperacion.num2 = n2;
+
+            //Console.WriteLine(realizarOperacion.num1 + " " + realizarOperacion.num2);
+            realizarOperacion.sumar(realizarOperacion.num1, realizarOperacion.num2);
+            realizarOperacion.mostrarResultados();
+            Console.ReadLine();
+
+        }
+    }
+}
