b2eb5213245224f614ddbdec04d198a9f13f2671
Daniela Montoya <danimdzj22@gmail.com>
2017-09-19 19:53:42 -0500

    Operacion Resta

--- ClaseNT/Calculos.cs
+++ ClaseNT/Calculos.cs
@@ -13,15 +13,36 @@ namespace ClaseNT
 
         //Metodo (Estructura orientada a objetos) modificador de acceso (public), retorno(void), nombre(sumar)
         short sumarNumeros = 0;
+        short restarNumeros = 0;
+        short multiNumeros = 0;
+        short diviNumeros = 0;
 
-        public void sumar(short numero1, short numero2)
+        public void sumar(short sumar1, short sumar2)
         {
-            sumarNumeros = (short) (numero1 + numero2);
+            sumarNumeros = (short) (sumar1 + sumar2);
         }
+
+        public void restar(short restar1, short restar2)
+        {
+            restarNumeros = (short)(restar1 - restar2);
+        }
+
+        /*public void multiplicar(short multi1, short multi2)
+        {
+            multiNumeros = (short)(multi1 * multi2);
+        }
+
+        public void dividir(short divi1, short divi2)
+        {
+            diviNumeros = (short)(divi1 / divi2);
+        }*/
 
         public void mostrarResultados()
         {
-            Console.WriteLine("Total: " + sumarNumeros);
+            Console.WriteLine("Total suma: " + sumarNumeros);
+            Console.WriteLine("Total resta: " + restarNumeros);
+            /*Console.WriteLine("Total multiplicación: " + multiNumeros);
+            Console.WriteLine("Total división: " + diviNumeros);*/
         }
 
     }
--- ClaseNT/Program.cs
+++ ClaseNT/Program.cs
@@ -11,20 +11,23 @@ namespace ClaseNT
         static void Main(string[] args)
         {
             Console.WriteLine("Numero uno: ");
-            short n1 = short.Parse(Console.ReadLine()); //Con esta linea se lleva el código
+            short s1 = short.Parse(Console.ReadLine()); //Con esta linea se lleva el código
 
             Console.WriteLine("Numero dos: ");
-            short n2 = short.Parse(Console.ReadLine());
+            short s2 = short.Parse(Console.ReadLine());
 
             //Instanciar la clase
             Calculos realizarOperacion = new Calculos(); //Constructor (Calculos() )
 
             //utilizamos el objeto (metodo aparece con un cubo (morado))
-            realizarOperacion.num1 = n1;
-            realizarOperacion.num2 = n2;
+            realizarOperacion.num1 = s1;
+            realizarOperacion.num2 = s2;
 
-            //Console.WriteLine(realizarOperacion.num1 + " " + realizarOperacion.num2);
+        
             realizarOperacion.sumar(realizarOperacion.num1, realizarOperacion.num2);
+            realizarOperacion.restar(realizarOperacion.num1, realizarOperacion.num2);
+            /*realizarOperacion.multiplicar(realizarOperacion.num1, realizarOperacion.num2);
+            realizarOperacion.dividir(realizarOperacion.num1, realizarOperacion.num2);*/
             realizarOperacion.mostrarResultados();
             Console.ReadLine();
 
